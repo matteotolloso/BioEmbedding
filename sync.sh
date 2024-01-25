2af967e346978a5f9c05a11b1d199f0302d00cdd
matteotolloso <matteotolloso@gmail.com>
2024-01-25 10:26:25 +0100

    updates

--- sync.sh
+++ sync.sh
@@ -1,2 +1,2 @@
-rsync -Przzuve ssh matteotolloso@anto.di.unipi.it:BioEmbedding ~/BioEmbedding
+# rsync -Przzuve ssh matteotolloso@anto.di.unipi.it:BioEmbedding ~/BioEmbedding
 rsync -Przzuve ssh ~/BioEmbedding matteotolloso@anto.di.unipi.it:BioEmbedding
